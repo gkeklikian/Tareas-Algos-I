fd3dcdb0090b8aead0cd2d6f8ec25cf368bd9312
gkeklikian <37820574+gkeklikian@users.noreply.github.com>
2018-03-26 22:54:09 -0300

    Add files via upload
    Ejercicio 24, guía 1.

---
+++ ejercicio24.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mensajes24.h"
+
+int main(void){
+	int a, b, x;
+	puts(msj_pedir_numero);
+	x = scanf("%d", &a);
+	if(x!=1){
+		fprintf(stderr, "%s: %s\n", msj_error, msj_error_ingreso);
+		return EXIT_FAILURE;
+	}
+	b=a%7;
+	switch(b){
+		case 1:
+			printf("%s\n", msj_domingo);
+			break;
+		case 2:
+			printf("%s\n", msj_lunes);
+			break;
+		case 3:
+			printf("%s\n", msj_martes);
+			break;
+		case 4:
+			printf("%s\n", msj_miercoles);
+			break;
+		case 5:
+			printf("%s\n", msj_jueves);
+			break;
+		case 6:
+			printf("%s\n", msj_viernes);
+			break;
+		case 0:
+			printf("%s\n", msj_sabado);
+			break;
+		default:
+			printf("error\n"); /* Si valide el numero ingresado, no tiene sentido que haya un default aca*/
+		}
+	return 0;
+}
